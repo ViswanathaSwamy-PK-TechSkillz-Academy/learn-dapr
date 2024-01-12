27b85aca693372c7f8ad340e619f2fd06f645e8b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-12 16:05:20 +0530

    Changes

---
+++ eStoresCookies.S2SI/eStoresCookies.ItemsReservationAPI/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace eStoresCookies.ItemsReservationAPI.Extensions;
+
+public static class ApplicationServicesExtensions
+{
+    public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
+    {
+        _ = services.AddControllers().AddDapr(opt => opt.UseJsonSerializationOptions(new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true,
+        }));
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        _ = services.AddEndpointsApiExplorer();
+        _ = services.AddSwaggerGen();
+
+        return services;
+    }
+
+}
---
+++ eStoresCookies.S2SI/eStoresCookies.ItemsReservationAPI/Extensions/HttpRequestPipelineExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace eStoresCookies.ItemsReservationAPI.Extensions;
+
+public static class HttpRequestPipelineExtensions
+{
+
+    public static WebApplication ConfigureHttpRequestPipeline(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
