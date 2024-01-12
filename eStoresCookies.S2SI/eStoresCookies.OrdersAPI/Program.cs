4a3937bb6ff021b8ce2f5229fac989385c6a5591
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-12 12:52:26 +0530

    Orders Service

--- eStoresCookies.S2SI/eStoresCookies.OrdersAPI/Program.cs
+++ eStoresCookies.S2SI/eStoresCookies.OrdersAPI/Program.cs
@@ -1,8 +1,14 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers().AddDapr(opt => opt.UseJsonSerializationOptions(new()
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    PropertyNameCaseInsensitive = true,
+}));
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,7 +22,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
